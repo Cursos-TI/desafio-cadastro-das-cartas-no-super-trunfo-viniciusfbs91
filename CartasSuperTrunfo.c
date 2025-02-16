d5deff7a88cd28e4e0bcc3cb1f53cd0c8ac98a7c
viniciusfbs91 <126971228+viniciusfbs91@users.noreply.github.com>
2025-02-16 21:32:29 +0000

    adicionado calculo pib percapita e densidade populacional

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,11 +7,21 @@
 //Teste larissa
 
 /*
+Novato: 
 O Usuário deve conseguir Criar cartas representando cidades, contendo as seguintes propriedades:
 População
 Área
 PIB
 Número de pontos turísticos
+*/
+
+/*
+Aventureiro
+Adicionar:
+Densidade Populacional: População dividida pela área da cidade.
+PIB per Capita: PIB total dividido pela população.
+
+Essas novas propriedades devem ser adicionadas às informações exibidas para cada cidade.
 */
 
 int main() {
@@ -30,27 +40,43 @@ int main() {
     
     char nome_cidade[50];
     int quantidade_populacao, numero_pontos_turisticos;
-    float area_territorial, valor_pib;
+    float area_territorial, valor_pib, densidade_populacional,valor_pib_percapita;
 
+    // Inicia o Programa com mensagem de bem vindo.
     printf("Bem Vindo ao Super Trunfo - Cadastro de Cartas\n");
     
+    // Solicita o nome da Cidade para o usuário.
     printf("Digite o Nome da Cidade:\n");
     scanf("%s",&nome_cidade);
 
-    printf("Digite a quantidade de População residente da Cidade %s:\n",nome_cidade);
+    // Solicita a População da Cidade para o usuário.
+    printf("Digite a quantidade de População residente da Cidade %s: (somente números Ex: 123456)\n",nome_cidade);
     scanf("%d",&quantidade_populacao);
 
+    // Solicita a área da Cidade para o usuário.
     printf("Digite a Área Territorial da Cidade %s (somente números Ex: 946049):\n",nome_cidade);
     scanf("%f",&area_territorial);
 
+    // Solicita o PIB da Cidade para o usuário.
     printf("Digite o Valor PIB da Cidade %s (somente números Ex: 80717.4):\n",nome_cidade);
     scanf("%f",&valor_pib);
 
+    // Realiza o Cálculo de Densidade e Pib Percapt
+    //Densidade Populacional: População dividida pela área da cidade.
+    //PIB per Capita: PIB total dividido pela população.
+
+    densidade_populacional = (float) quantidade_populacao / area_territorial;
+    valor_pib_percapita = (float) valor_pib / quantidade_populacao;
+
+    // Apresenta a mensagem final para o usuário.
     printf("\nCarta Cadastrada com Sucesso!\n");
     printf("Nome: %s\n",nome_cidade);
     printf("População: %d\n",quantidade_populacao);
     printf("Área: %f\n",area_territorial);
     printf("PIB: %f\n",valor_pib);
+    printf("Densidade Populacional: %.2f\n",densidade_populacional);
+    printf("PIB per Capita: %.2f\n",valor_pib_percapita);
+
 
     return 0;
 }
